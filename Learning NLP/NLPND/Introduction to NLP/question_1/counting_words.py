0082382442153c9a6985f169e63187bd524e9340
Ajay-2007 <32496279+Ajay-2007@users.noreply.github.com>
2019-09-02 12:53:22 -0700

    Add files via upload

---
+++ Learning NLP/NLPND/Introduction to NLP/question_1/counting_words.py
@@ -0,0 +1,48 @@
+"""Count words."""
+import re
+
+def count_words(text):
+    """Count how many times each unique word occurs in text."""
+    counts = dict()  # dictionary of { <word>: <count> } pairs to return
+
+    # TODO: Convert to lowercase
+    text = text.lower()
+
+    # TODO: Split text into tokens (words), leaving out punctuation
+    # (Hint: Use regex to split on non-alphanumeric characters)
+    words = re.split(r'[^a-zA-Z\d\s:]', text)
+
+    # TODO: Aggregate word counts using a dictionary
+    for i in range(len(words)):
+        words[i] = words[i].split()
+
+    for sentence in words:
+        for word in sentence:
+            if word not in counts:
+                counts[word] = 0
+
+    for sentence in words:
+        for word in sentence:
+            if word in counts:
+                counts[word] += 1
+
+    return counts
+
+
+def test_run():
+    with open("input.txt", "r") as f:
+        text = f.read()
+        counts = count_words(text)
+        sorted_counts = sorted(counts.items(), key=lambda pair: pair[1], reverse=True)
+
+        print("10 most common words:\nWord\tCount")
+        for word, count in sorted_counts[:10]:
+            print("{}\t{}".format(word, count))
+
+        print("\n10 least common words:\nWord\tCount")
+        for word, count in sorted_counts[-10:]:
+            print("{}\t{}".format(word, count))
+
+
+if __name__ == "__main__":
+    test_run()
